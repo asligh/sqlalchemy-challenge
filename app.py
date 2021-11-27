2b9bd80f5ca1bfef99090a2541a1cbee3796eac9
Ashley Sligh <asligh1@gmail.com>
2021-11-27 15:00:22 -0500

    Part 2 and cleanup

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import numpy as np
 
 import sqlalchemy
+import datetime as dt
+
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
@@ -11,7 +13,7 @@ from flask import Flask, jsonify
 #################################################
 # Database Setup
 #################################################
-engine = create_engine("sqlite:///titanic.sqlite")
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
 
 # reflect an existing database into a new model
 Base = automap_base()
@@ -19,7 +21,8 @@ Base = automap_base()
 Base.prepare(engine, reflect=True)
 
 # Save reference to the table
-Passenger = Base.classes.passenger
+Measurements = Base.classes.measurement
+Stations     = Base.classes.station
 
 #################################################
 # Flask Setup
@@ -36,50 +39,142 @@ def welcome():
     """List all available api routes."""
     return (
         f"Available Routes:<br/>"
-        f"/api/v1.0/names<br/>"
-        f"/api/v1.0/passengers"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/<start><br/>"
+        f"/api/v1.0/<start>/<end>"
     )
 
-
-@app.route("/api/v1.0/names")
-def names():
+@app.route("/api/v1.0/precipitation")
+def precipitation():
     # Create our session (link) from Python to the DB
     session = Session(engine)
 
-    """Return a list of all passenger names"""
-    # Query all passengers
-    results = session.query(Passenger.name).all()
+    q = session.query(Measurements.date).order_by(Measurements.date.desc()).first()
+    end_date_obj   = dt.datetime.strptime(q.date,'%Y-%m-%d')
+    start_date_obj  = dt.datetime.strptime(str(end_date_obj - dt.timedelta(days=365)), '%Y-%m-%d %H:%M:%S').date()
+
+    sel = [Measurements.date,Measurements.prcp]
+
+    results = session.query(*sel).filter(Measurements.date >= start_date_obj).all()
+
+    results_dict = {}
+
+    for date, prcp in results:
+
+        measurement_date = date
+        measurement_prcp = prcp     
+
+        results_dict["date"] = measurement_date
+        results_dict["prcp"] = measurement_prcp
+        results_dict.update({measurement_date:measurement_prcp})
+
+    session.close()
+
+    return jsonify(results_dict)
+
+@app.route("/api/v1.0/stations")
+def stations():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    """Return a list of all stations"""
+    results = session.query(Stations.name).all()
 
     session.close()
 
     # Convert list of tuples into normal list
-    all_names = list(np.ravel(results))
+    station_names = list(np.ravel(results))
 
-    return jsonify(all_names)
+    return jsonify(station_names)
 
 
-@app.route("/api/v1.0/passengers")
-def passengers():
-    # Create our session (link) from Python to the DB
+@app.route("/api/v1.0/tobs")
+def tobs():
+     # Create our session (link) from Python to the DB
     session = Session(engine)
 
-    """Return a list of passenger data including the name, age, and sex of each passenger"""
-    # Query all passengers
-    results = session.query(Passenger.name, Passenger.age, Passenger.sex).all()
+    """Return a list of tobs"""
+
+    records = session.query(Stations.id,Stations.station,func.count(Stations.id)).\
+                      filter(Measurements.station == Stations.station).\
+                      group_by(Stations.id,Stations.name).\
+                      order_by(func.count(Stations.id).desc()).\
+                      all()
+
+    most_active_station = records[0][1]
+
+    end_date = session.query(Measurements.date).\
+                       filter(Measurements.station == Stations.station).\
+                       filter(Measurements.station == most_active_station).\
+                       order_by(Measurements.date.desc()).\
+                       first()
+
+    end_date_obj   = dt.datetime.strptime(end_date.date,'%Y-%m-%d')
+    start_date_obj = dt.datetime.strptime(str(end_date_obj - dt.timedelta(days=365)), '%Y-%m-%d %H:%M:%S').date()
+
+    records = session.query(Measurements.date, Stations.station, Measurements.tobs).\
+                      filter(Measurements.station == Stations.station).\
+                      filter(Measurements.station == most_active_station).\
+                      filter(Measurements.date >= start_date_obj).\
+                      order_by(Measurements.date).\
+                      all()
 
     session.close()
 
-    # Create a dictionary from the row data and append to a list of all_passengers
-    all_passengers = []
-    for name, age, sex in results:
-        passenger_dict = {}
-        passenger_dict["name"] = name
-        passenger_dict["age"] = age
-        passenger_dict["sex"] = sex
-        all_passengers.append(passenger_dict)
+    all_tobs = []
 
-    return jsonify(all_passengers)
+    for date, station, tobs in records:
+         tobs_dict = {}
+         tobs_dict["date"]    = date
+         tobs_dict["station"] = station
+         tobs_dict["tobs"]     = tobs
+         all_tobs.append(tobs_dict)
 
+    return jsonify(all_tobs)
+
+@app.route("/api/v1.0/<start>")
+def get_tobs_by_start_date(start):
+     
+      # Create our session (link) from Python to the DB
+    session = Session(engine)
+     
+    start_date_obj = dt.datetime.strptime(start, '%Y-%m-%d')
+
+    records = session.query(func.min(Measurements.tobs), func.avg(Measurements.tobs), func.max(Measurements.tobs)).\
+            filter(Measurements.station == Stations.station).\
+            filter(Measurements.date >= start_date_obj).\
+            order_by(Measurements.date).\
+            all()
+
+    rec_list = list(np.ravel(records))
+
+    session.close()    
+
+    return jsonify(rec_list)
+    
+@app.route("/api/v1.0/<start>/<end>")
+def get_tobs_by_start_end_date(start,end):
+     
+      # Create our session (link) from Python to the DB
+    session = Session(engine)
+     
+    start_date_obj = dt.datetime.strptime(start, '%Y-%m-%d')
+    end_date_obj   = dt.datetime.strptime(end, '%Y-%m-%d')   
+
+    records = session.query(func.min(Measurements.tobs), func.avg(Measurements.tobs), func.max(Measurements.tobs)).\
+            filter(Measurements.station == Stations.station).\
+            filter(Measurements.date >= start_date_obj).\
+            filter(Measurements.date <= end_date_obj).\
+            order_by(Measurements.date).\
+            all()
+
+    rec_list = list(np.ravel(records))
+
+    session.close()    
+
+    return jsonify(rec_list)
 
 if __name__ == '__main__':
     app.run(debug=True)
